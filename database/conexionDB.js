7110366ae556cb38bcadd1d7d7fc75a63f30a139
YasserRamos <yasseromar_27@hotmail.com>
2026-02-11 01:09:56 -0500

    Cambio de base de datos de local a nube Ver 1.5

--- database/conexionDB.js
+++ database/conexionDB.js
@@ -2,11 +2,19 @@
 import mysql from "mysql2/promise";
 
 const conexion = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "yassy0127", // cámbialo si tienes contraseña
-  database: "crudprueba" // cambia esto por tu base real
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
+// const conexion = await mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "yassy0127",
+//   database: "crudprueba"
+// });
 
 console.log("📦 Conectado a MySQL correctamente");
 
