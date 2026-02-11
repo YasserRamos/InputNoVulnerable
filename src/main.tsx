143c89f8c76ed774527ae6c0af52e495289e883d
YasserRamos <yasseromar_27@hotmail.com>
2026-02-10 19:24:20 -0500

    Corrección donde HTMLElement puede ser null Ver 1.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // Solo aquí
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root") as HTMLElement;
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
